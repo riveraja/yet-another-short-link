aad816a1b0aebd768d99e0b1503cc22842d97c1e
Jericho Rivera <jerichorivera@gmail.com>
2024-04-10 18:20:16 +0800

    finalize login action

--- utils/hashing.ts
+++ utils/hashing.ts
@@ -1,13 +1,11 @@
 
 export const createPasswordHash = async (password: string) => {
-    const hashedPassword: string = await Bun.password.hash(password, {
+    return await Bun.password.hash(password, {
         algorithm: 'bcrypt',
         cost: 4
     })
-    return hashedPassword
 }
 
 export const verifyPasswordHash = async (password: string, hashedPassword: string) => {
-    const isMatch: boolean = await Bun.password.verify(password, hashedPassword)
-    return isMatch
+    return await Bun.password.verify(password, hashedPassword)
 }
