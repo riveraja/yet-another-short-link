66bb0ec2b857cef89f7333e044216be4ed16a054
Jericho Rivera <jerichorivera@gmail.com>
2024-04-11 18:51:06 +0800

    add migration meta updates

---
+++ drizzle/0001_nervous_ben_parker.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "urls" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"long_url" text NOT NULL,
+	"short_url" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"expires_on" timestamp NOT NULL,
+	"created_by" text NOT NULL
+);
