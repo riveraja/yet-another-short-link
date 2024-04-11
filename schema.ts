b501474230c9a089df75a9295af04f3eaf372ef5
Jericho Rivera <jerichorivera@gmail.com>
2024-04-11 18:46:42 +0800

    add urls migrations

--- schema.ts
+++ schema.ts
@@ -10,3 +10,12 @@ export const users = pgTable('users', {
 }, (t) => ({
     uniqueKey1: unique().on(t.user_id, t.email),
 }))
+
+export const urls = pgTable('urls', {
+    id: uuid('id').default(sql`gen_random_uuid()`).primaryKey(),
+    long_url: text('long_url').notNull(),
+    short_url: text('short_url').notNull(),
+    created_at: timestamp('created_at').default(sql`now()`),
+    expires_on: timestamp('expires_on').notNull(),
+    created_by: text('created_by').notNull()
+})
